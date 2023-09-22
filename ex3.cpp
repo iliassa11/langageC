ff3093021a6bb59fe62a2859075b879688893d79
iliassa11 <abdou.iliassa@lyceejulesfil.fr>
2023-09-22 14:53:37 +0200

    test 3

--- ex3.cpp
+++ ex3.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main()
 {
    int age_utilisateur(18);
-   cout<<"L'age de l'utilisateur est:";
+   cout<<"L'age de l'utilisateur est: ";
    cout<<age_utilisateur;
-   cout<<"ans."<<endl;
+   cout<<" ans."<<endl;
    return 0; 
 }
