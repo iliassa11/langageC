c90633cbafabdaa1855295ee3db9a574cb45e9dc
iliassa11 <abdou.iliassa@lyceejulesfil.fr>
2023-09-22 14:47:19 +0200

    test 3

--- C++.cpp
+++ C++.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-using namespace std;
+using namespace std;   //Ceci est commentaire sur une ligne
 int main()
+/*Ceci est un commentaire
+sur plusieurs lignes*/
 {
-    std::cout <<"Hello,World!"<<endl;
-    return 0;
+    std::cout <<"Hello,World!"<<endl; //Affiche Bonjours, le Monde
+    return 0; /*Affiche Bonjours, le monde */
 }
-//ex1
+//ex2
---
+++ ex3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;   
+int main()
+{
+   int age_utilisateur(18);
+   cout<<"L'age de l'utilisateur est:";
+   cout<<age_utilisateur;
+   cout<<"ans."<<endl;
+   return 0; 
+}
